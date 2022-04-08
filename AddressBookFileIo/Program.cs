7a6abf4e5d3b24458f462c5f2865ca41244a3d76
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-04-08 19:22:24 +0530

    UC-6 Dictionary

--- AddressBookFileIo/Program.cs
+++ AddressBookFileIo/Program.cs
@@ -4,101 +4,45 @@ namespace AddressBook
 {
     class Program
     {
-        //declaration
-        AddressBook book;
-
-        public Program()
-        {
-             book = new AddressBook();
-        }
-
         static void Main(string[] args)
         {
-            //creation of object
-            Program p = new Program();
-            //welcome message
-            Console.WriteLine("Hello, Welcome to Address Book!");
-            bool loop = true;
-            while (loop)
+            //Adding Contact
+            Console.WriteLine("------------Welcome To Adderess Book Program-------------");
+            Console.WriteLine("1.Enter to add the details\n2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove or Delete the Contact details");
+            Console.WriteLine("5.Multiple Contact Details /Dictionary Added");
+            Console.WriteLine("Enter a option");
+            AddressBook addressBook = new AddressBook();
+            switch (Console.ReadLine())
             {
-                Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact.\n3. Delete Contact \n4. Exit.");
-                int option = int.Parse(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        Console.WriteLine("Enter the person details to be added in the address book");
-                        Console.WriteLine("First Name");
-                        string FirstName = Console.ReadLine();
-                        Console.WriteLine("Last Name");
-                        string LastName = Console.ReadLine();
-                        Console.WriteLine("Address");
-                        string Address = Console.ReadLine();
-                        Console.WriteLine("City");
-                        string City = Console.ReadLine();
-                        Console.WriteLine("State");
-                        string State = Console.ReadLine();
-                        Console.WriteLine("Zip code");
-                        string ZipCode = Console.ReadLine();
-                        Console.WriteLine("Phone Number");
-                        string PhoneNumber = Console.ReadLine();
-                        Console.WriteLine("Email");
-                        string Email = Console.ReadLine();
-                        if (p.book.AddContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email))
-                        {
-                            Console.WriteLine("Contact added successfully");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Contact already exists");
-                            break;
-                        }
-                    case 2:
-                        Console.WriteLine("Enter the first name of the contact to be edited ");
-                        string name = Console.ReadLine();
-                        Contact c = p.book.FindContact(name);
-                        if (c == null)
-                        {
-                            Console.WriteLine("Address for {0} count not be found.", name);
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("New Last Name");
-                            c.LastName = Console.ReadLine();
-                            Console.WriteLine("New Address");
-                            c.Address = Console.ReadLine();
-                            Console.WriteLine("New City");
-                            c.City = Console.ReadLine();
-                            Console.WriteLine("New State");
-                            c.State = Console.ReadLine();
-                            Console.WriteLine("New Zip code");
-                            c.ZipCode = Console.ReadLine();
-                            Console.WriteLine("New Phone Number");
-                            c.PhoneNumber = Console.ReadLine();
-                            Console.WriteLine("New Email");
-                            c.Email = Console.ReadLine();
-                            Console.WriteLine("Details updated for " + name);
-                            break;
-                        }
-                    case 3:
-                        Console.WriteLine("Enter the first name of the contact to be deleted ");
-                        string name1 = Console.ReadLine();
-                        if (p.book.RemoveContact(name1))
-                        {
-                            Console.WriteLine("Contact removed successfully");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Contact not found");
-                            break;
-                        }
-                    case 4:
-                        loop = false;
-                        break;
-                }
+                case "1":
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                case "2":
+                    addressBook.GetContact();
+                    addressBook.EditContact();
+                    addressBook.ListContact();
+                    break;
+                case "3":
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                case "4":
+                    addressBook.GetContact();
+                    addressBook.DeletePeople();
+                    break;
+                case "5":
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
             }
+
         }
     }
 }
